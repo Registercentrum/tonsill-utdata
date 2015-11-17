3810c954e9f29668aacfde04f59a402eab133f83
Fredrik Lundberg <fredrik.lundberg@registercentrum.se>
2015-11-17 15:57:04 +0100

    Added renderer for confidence intervals
    Currently a bit glitchy when disabling and enabling

--- js/main.js
+++ js/main.js
@@ -52,8 +52,8 @@ var TonsillWidget = {
 					retData[i.key]['cTotal' + (unit == 0 ? 'R' : '')] = i.value[0].value;
 					retData[i.key]['cBleed' + (unit == 0 ? 'R' : '')] = (conf.TT ? i.value[1].value : 0) + (conf.TE ? i.value[2].value : 0);
 					retData[i.key]['sBleed' + (unit == 0 ? 'R' : '')] = !conf.TT && !conf.TE ? 0 : i.value[TonsillWidget.getTTorTEValue(conf)].value * 100;
+					retData[i.key]['ciBleed' + (unit == 0 ? 'R' : '')] = !conf.TT && !conf.TE ? 0 : i.value[TonsillWidget.getTTorTEValue(conf) + 3].value * 100;;
 					retData[i.key]['method'] = !conf.TE && !conf.TT ? '' : conf.TE ? (conf.TT ? 'TE+TT' : 'TE') : 'TT';
-
 				});
 				if (unit !== 0) {
 					conf.unit = 0;
@@ -79,6 +79,88 @@ var TonsillWidget = {
 	getTTorTEValue: function(conf) {
 		return conf.TT ? (conf.TE ? 3 : 4) : 5;
 	},
+	getErrorPathAttributes: function(barSprite, barConfig, deviation, lineConf) {
+		var conf = Ext.isObject(lineConf) ? lineConf : {},
+			errorWidth = (conf.errorWidth || 20) / 2,
+			lineWidth = (conf.lineWidth || 2) / 2,
+			barX = barConfig.x + barConfig.width / 2,
+			barY = barConfig.y,
+			maxHeight = barSprite.attr.innerHeight;
+		return {
+			path: [
+				'M', barX, barY,
+				'V', Math.min(maxHeight - lineWidth, barY + deviation - lineWidth), //Line to top bar
+				'M', barX - errorWidth, Math.min(maxHeight - lineWidth, barY + deviation - lineWidth),
+				'H', errorWidth + barX, //Top error bar
+				'M', barX, barY,
+				'V', Math.max(barY - deviation + lineWidth, lineWidth),
+				'M', barX - errorWidth, Math.max(barY - deviation + lineWidth, lineWidth),
+				'H', errorWidth + barX // Bottom error bar
+			],
+			stroke: conf.stroke || '#000',
+			lineWidth: conf.lineWidth || 2,
+			zIndex: conf.zIndex || 10000
+		};
+	},
+	errorChartRenderer: function(sprite, config, rendererData, index) {
+		var me = TonsillWidget,
+			store = rendererData.store,
+			storeItems = store.getData().items,
+			last = storeItems.length - 1,
+			record = storeItems[index],
+			surface = sprite.getParent(),
+			errorSprites = surface.myErrorSprites,
+			scale = sprite.attr.scalingY,
+			exactIndex = index + sprite._field,
+			isRiket = (/^sBleedR$/).test(sprite._field),
+			deviation, errorSprite, i, isLast, retObject;
+
+		isLast = index === rendererData.store.count() - 1;
+		retObject = {
+			fillOpacity: isLast ? 0.5 : 1,
+			strokeOpacity: isLast ? 0 : 1,
+		};
+		if (!record || !Ext.getCmp('cb-ci').getValue()) {
+			// Hides all sprites if there are no records... And adds a text sprite
+			if (errorSprites && !surface.mySpritesHidden) {
+				Ext.each(errorSprites, function(es) {
+					es.hide();
+				});
+				// surface.mySpritesHidden = true;
+			}
+			return retObject;
+		}
+		surface.mySpritesHidden = false;
+
+		deviation = record.get(isRiket ? 'ciBleedR' : 'ciBleed') * scale;
+
+		if (!errorSprites) {
+			errorSprites = surface.myErrorSprites = [];
+		}
+		errorSprite = errorSprites[exactIndex];
+		if (!errorSprite) {
+			errorSprite = errorSprites[exactIndex] = surface.add({
+				type: 'path'
+			});
+		} else {
+			errorSprite.show();
+		}
+		errorSprite.setAttributes(me.getErrorPathAttributes(sprite, config, deviation, {
+			lineWidth: 1,
+			stroke: '#58585a'
+		}));
+		if (index === last) {
+			for (i = last + 1; i < errorSprites.length; i++) {
+				errorSprites[i].hide();
+			}
+		}
+		return retObject;
+		//Adjust width
+		// return {
+		// width: config.width / 1.25,
+		// x: config.x + (config.width - config.width / 1.25) / 2
+		// };
+	},
 	_initComponents: function() {
 		var chart, indicatorCombo, unitCombo, mainStore, ct, combosCt, legend;
 		var conf = TonsillWidget.config;
@@ -97,10 +179,7 @@ var TonsillWidget = {
 			margin: '0 0 39px 0'
 		});
 		legend = Ext.create('Ext.chart.Legend', {
-			// tpl: ['<div class="ton-legend-container">', '<tpl for=".">', '<div class="ton-legend-item">', '<span class="ton-legend-item-marker" ', 'style="background:{mark};">', '</span>{name}', '</div>', '</tpl>', '</div>'],
-			// position: 'top',
 			flex: 1,
-			// renderTo: Ext.getBody()
 		});
 
 		unitCombo = TonsillWidget._unitCombo = Ext.create({
@@ -137,7 +216,9 @@ var TonsillWidget = {
 				focus: function(cb) {
 					// cb.clearValue();
 					cb.selectText();
-					cb.expand();
+					if (!cb.isExpanded) {
+						cb.expand();
+					}
 				}
 			},
 			value: conf.unit,
@@ -260,13 +341,7 @@ var TonsillWidget = {
 								storeItem.get('method')));
 						}
 					},
-					renderer: function(sprite, config, rendererData, index) {
-						var isLast = index === rendererData.store.count() - 1;
-						return {
-							fillOpacity: isLast ? 0.5 : 1,
-							strokeOpacity: isLast ? 0 : 1,
-						}
-					}
+					renderer: TonsillWidget.errorChartRenderer
 				}
 				/*, {
 								type: 'bar',
